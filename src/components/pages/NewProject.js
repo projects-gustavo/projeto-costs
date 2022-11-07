405455dcfdad7ed0f31f1e27210e2e51185366f8
Gustavo Rodrigues Martins <79873612+projects-gustavo@users.noreply.github.com>
2022-11-07 18:50:06 -0300

    Update NewProject.js

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -2,7 +2,6 @@ import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
-    
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
